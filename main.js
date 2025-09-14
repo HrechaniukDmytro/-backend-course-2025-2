61a5a3091db2cee21e120ad8efcd0ccd10141d93
HrechaniukDmytro <dmytro.hrechaniuk@lnu.edu.ua>
2025-09-14 21:28:18 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
-ECHO is on.
+// 1. Константи
+const currency = "UAH";
+const exact_date = "2024-09-10";
+
+// 2. Базова адреса (корінь)
+const baseUrl = "https://bank.gov.ua/ua/open-data/api/ovdp";
+
+// 3. Створюємо URL-об'єкт
+const url = new URL(baseUrl);
+
+// 4. Додаємо параметри пошуку (query string)
+url.searchParams.append("date", exact_date);
+url.searchParams.append("valcode", currency);
+
+// 5. Виводимо у консоль
+console.log("Сформований URL:", url.toString());
